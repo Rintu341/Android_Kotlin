c563537d8c60e1ec0102fb9649b34d1c95546bcd
sujan <sujanrintu@gmail.com>
2024-01-07 20:18:24 +0530

    Add Top App Bar in Shopping list

--- shopingListApp/app/src/main/java/com/example/shopinglistapp/ShopingListAppUI.kt
+++ shopingListApp/app/src/main/java/com/example/shopinglistapp/ShopingListAppUI.kt
@@ -16,107 +16,91 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Create
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.shopinglistapp.model.ShoppingListItem
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ShoppingListApp() {
-    var isOpenDialog by remember {
+    var isOpenDialog by rememberSaveable {
         mutableStateOf(false)
     }
-    var shoppingData by remember {
+    var shoppingData by rememberSaveable {
         mutableStateOf(listOf<ShoppingListItem>())
     }
-    var itemName by remember {
+    var itemName by rememberSaveable {
         mutableStateOf("")
     }
-    var itemQnt by remember {
+    var itemQnt by rememberSaveable {
         mutableStateOf("")
     }
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(all = 12.dp)
-    ) {
-
-        Text(
-            text = "ShoppingList App",
-            fontSize = MaterialTheme.typography.titleLarge.fontSize,
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .padding(12.dp),
-            fontFamily = FontFamily.Default,
-            color = MaterialTheme.colorScheme.primary
-        )
-        LazyColumn(
-            modifier = Modifier.fillMaxWidth(1f),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            items(shoppingData)
-            {item ->
-                if(item.isEdit)
-                {
-                    ShoppingItemEditor(item = item, onEditComplete ={
-                            itemName, itemQuantity ->
-                        shoppingData = shoppingData.map { it.copy(isEdit = false) }
-                        val itemItem = shoppingData.find { it.id == item.id }
-                        itemItem?.let{
-                            it.itemName = itemName
-                            it.quantity = itemQuantity
-                        }
-                    } )
-                    Log.d("Update","update")
-                }else {
-                    ItemUI(item,
-                        onEditClick = {
-                            shoppingData = shoppingData.map{it.copy(isEdit = it.id ==item.id)}
-                        },
-                        onDeleteClick = {
-                            shoppingData = shoppingData - item
-                        }
-                    )
-                    Log.d("Show","show")
-                }
-            }
-        }
-        Row(
-            modifier = Modifier
-                .fillMaxSize(1f)
-                .padding(8.dp),
-            verticalAlignment = Alignment.Bottom,
-            horizontalArrangement = Arrangement.End,
-        ) {
+    Scaffold(
+        topBar = {
+                 TopAppBar(
+                     title = {
+                     Text("Shopping List",
+                         fontFamily = FontFamily.Default,
+                         fontWeight = FontWeight.Bold
+                     )
+                     },
+                     colors = topAppBarColors(
+                         containerColor = MaterialTheme.colorScheme.primaryContainer,
+                         titleContentColor = MaterialTheme.colorScheme.primary,
+                     ),
+                     navigationIcon = {
+                         IconButton(onClick = { /*TODO*/ }) {
+                             Icon(Icons.Filled.ArrowBack, contentDescription = null)
+                         }
+                     },
+                     actions = {
+                         IconButton(onClick = { /*TODO*/ }) {
+                             Icon(Icons.Filled.Search, contentDescription = null)
+                         }
+                         IconButton(onClick = { /*TODO*/ }) {
+                             Icon(Icons.Filled.Menu, contentDescription = null)
+                         }
+                     }
+                 )
+        },
+        floatingActionButton = {
             FloatingActionButton(
                 onClick = { isOpenDialog = !isOpenDialog },
                 shape = RoundedCornerShape(30.dp),
 
                 ) {
-                IconButton(onClick = {isOpenDialog = !isOpenDialog}) {
+                IconButton(onClick = { isOpenDialog = !isOpenDialog }) {
                     Icon(
                         Icons.Filled.Add,
                         contentDescription = "Add Item",
@@ -124,6 +108,49 @@ fun ShoppingListApp() {
                     )
                 }
             }
+        },
+            floatingActionButtonPosition =  FabPosition.End
+    ) {values->
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(values)
+    ) {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                items(shoppingData)
+                { item ->
+                    if (item.isEdit) {
+                        ShoppingItemEditor(item = item, onEditComplete = { itemName, itemQuantity ->
+                            shoppingData = shoppingData.map { it.copy(isEdit = false) }
+                            val itemItem = shoppingData.find { it.id == item.id }
+                            itemItem?.let {
+                                it.itemName = itemName
+                                it.quantity = itemQuantity
+                            }
+                        })
+                        Log.d("Update", "update")
+                    } else {
+                        ItemUI(item,
+                            onEditClick = {
+                                shoppingData =
+                                    shoppingData.map { it.copy(isEdit = it.id == item.id) }
+                            },
+                            onDeleteClick = {
+                                shoppingData = shoppingData - item
+                            }
+                        )
+                        Log.d("Show", "show")
+                    }
+                }
+            }
+        }
         }
         if (isOpenDialog) {
             AlertDialog(
@@ -179,18 +206,17 @@ fun ShoppingListApp() {
             )
         }
     }
-}
 
 @Composable
 fun ShoppingItemEditor(item:ShoppingListItem,onEditComplete:(String,Int)->Unit)
 {
-    var itemName by remember{
+    var itemName by rememberSaveable{
         mutableStateOf(item.itemName)
     }
-    var itemQuantity by remember {
+    var itemQuantity by rememberSaveable {
         mutableStateOf(item.quantity.toString())
     }
-    var isEditing by remember {
+    var isEditing by rememberSaveable {
         mutableStateOf(item.isEdit)
     }
     Row (
@@ -273,44 +299,45 @@ fun ItemUI(myList : ShoppingListItem,onEditClick:()->Unit,onDeleteClick:()->Unit
 @Preview(showBackground = true)
 @Composable
 fun ShoppingListAppPreview() {
-    var name by remember{
-        mutableStateOf("")
-    }
-
-    Row (
-        modifier = Modifier
-            .fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement =  Arrangement.SpaceEvenly
-    ){
-        Column (
-            modifier = Modifier.weight(3f)
-        ){
-            TextField(value = name,
-                onValueChange ={name = it} ,
-                singleLine =  true,
-                modifier = Modifier
-                    .wrapContentSize()
-                    .padding(8.dp),
-            )
-            TextField(value = name,
-                onValueChange ={name = it} ,
-                singleLine =  true,
-                modifier = Modifier
-                    .wrapContentSize()
-                    .padding(8.dp),
-            )
-        }
-        Button(onClick = {
-//            isEditing = false
-//            onEditComplete(itemName,itemQuantity.toIntOrNull()?:1)
-        },
-            modifier = Modifier.weight(1f),
-            colors = ButtonDefaults.buttonColors(Color.Blue),
-        ) {
-            Text(
-                "Save",
-            )
-        }
-    }
+    ShoppingListApp()
+//    var name by remember{
+//        mutableStateOf("")
+//    }
+//
+//    Row (
+//        modifier = Modifier
+//            .fillMaxWidth(),
+//        verticalAlignment = Alignment.CenterVertically,
+//        horizontalArrangement =  Arrangement.SpaceEvenly
+//    ){
+//        Column (
+//            modifier = Modifier.weight(3f)
+//        ){
+//            TextField(value = name,
+//                onValueChange ={name = it} ,
+//                singleLine =  true,
+//                modifier = Modifier
+//                    .wrapContentSize()
+//                    .padding(8.dp),
+//            )
+//            TextField(value = name,
+//                onValueChange ={name = it} ,
+//                singleLine =  true,
+//                modifier = Modifier
+//                    .wrapContentSize()
+//                    .padding(8.dp),
+//            )
+//        }
+//        Button(onClick = {
+////            isEditing = false
+////            onEditComplete(itemName,itemQuantity.toIntOrNull()?:1)
+//        },
+//            modifier = Modifier.weight(1f),
+//            colors = ButtonDefaults.buttonColors(Color.Blue),
+//        ) {
+//            Text(
+//                "Save",
+//            )
+//        }
+//    }
 }
